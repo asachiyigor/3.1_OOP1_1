622de2b904e2775c99fc406ab3510d69329bb508
asachiyigor <asachiyigor@gmail.com>
2022-04-02 11:43:55 +0300

    100%

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,78 +1,110 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package ru.netology.domain;
 
 public class Radio {
-
     private int maxStationNumber = 10;
     private int minVolume = 0;
     private int maxVolume = 100;
     private int currentStationNumber = 0;
     private int currentVolume = 0;
 
-    public Radio() {
-
-    }
-
     public Radio(int maxStationNumber) {
         this.maxStationNumber = maxStationNumber;
     }
 
     public int getMaxStationNumber() {
-        return maxStationNumber;
+        return this.maxStationNumber;
     }
 
     public int getCurrentStationNumber() {
-        return currentStationNumber;
+        return this.currentStationNumber;
     }
 
     public int getCurrentVolume() {
-        return currentVolume;
+        return this.currentVolume;
     }
 
     public void setCurrentStationNumber(int NewCurrentStationNumber) {
-        if (NewCurrentStationNumber > (maxStationNumber)) {
-            return;
+        if (NewCurrentStationNumber <= this.maxStationNumber) {
+            if (NewCurrentStationNumber >= 0) {
+                this.currentStationNumber = NewCurrentStationNumber;
+            }
         }
-        if (NewCurrentStationNumber < 0) {
-            return;
-        }
-        this.currentStationNumber = NewCurrentStationNumber;
     }
 
     public void next() {
-        if (currentStationNumber == (maxStationNumber - 1)) {
-            setCurrentStationNumber(0);
+        if (this.currentStationNumber == this.maxStationNumber - 1) {
+            this.setCurrentStationNumber(0);
         } else {
-            setCurrentStationNumber(currentStationNumber + 1);
+            this.setCurrentStationNumber(this.currentStationNumber + 1);
         }
+
     }
 
     public void prev() {
-        if (currentStationNumber == maxStationNumber) {
-            setCurrentStationNumber(maxStationNumber - 1);
+        if (this.currentStationNumber == this.maxStationNumber) {
+            this.setCurrentStationNumber(this.maxStationNumber - 1);
         } else {
-            setCurrentStationNumber(currentStationNumber - 1);
+            this.setCurrentStationNumber(this.currentStationNumber - 1);
         }
+
     }
 
     public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume > maxVolume) {
-            return;
+        if (newCurrentVolume <= this.maxVolume) {
+            if (newCurrentVolume >= this.minVolume) {
+                this.currentVolume = newCurrentVolume;
+            }
         }
-        if (newCurrentVolume < minVolume) {
-            return;
-        }
-        this.currentVolume = newCurrentVolume;
     }
 
     public void volumeUp() {
-        if (currentVolume < 100) {
-            this.currentVolume = currentVolume + 1;
+        if (this.currentVolume < 100) {
+            ++this.currentVolume;
         }
+
     }
 
     public void volumeDown() {
-        if (currentVolume > 0) {
-            this.currentVolume = currentVolume - 1;
+        if (this.currentVolume > 0) {
+            --this.currentVolume;
         }
+
     }
+
+    public Radio() {
+    }
+
+    public Radio(int maxStationNumber, int minVolume, int maxVolume, int currentStationNumber, int currentVolume) {
+        this.maxStationNumber = maxStationNumber;
+        this.minVolume = minVolume;
+        this.maxVolume = maxVolume;
+        this.currentStationNumber = currentStationNumber;
+        this.currentVolume = currentVolume;
+    }
+
+    public int getMinVolume() {
+        return this.minVolume;
+    }
+
+    public int getMaxVolume() {
+        return this.maxVolume;
+    }
+
+    public void setMaxStationNumber(int maxStationNumber) {
+        this.maxStationNumber = maxStationNumber;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
 }
